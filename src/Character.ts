ee91b05c68b3e193e485b6c150883b3c8268655a
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-07-10 21:20:42 -0300

    Requisito 8 completo - interface SimpleFighter, além do requisito 7 quase completo - classe Character, faltando validação de energy

---
+++ src/Character.ts
@@ -0,0 +1,91 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+export default class Character implements Fighter {
+  race: Race;
+  archetype: Archetype;
+  maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._dexterity = getRandomInt(1, 10);
+    this.race = new Elf(name, this._dexterity);
+    this.archetype = new Mage(name);
+    this.maxLifePoints = (this.race.maxLifePoints) / 2;
+    this._lifePoints = this.maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this.archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  get defense() {
+    return this._defense;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  get energy() {
+    return this._energy;
+  }
+
+  // get energy.amount() {
+  //   return this._energy.amount
+  // }
+
+  attack(enemy: Fighter): void {
+    let { lifePoints } = enemy;
+    lifePoints -= this.strength;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  special(enemy: Fighter): void {
+    console.log('HADOUKEN!');
+    let { lifePoints } = enemy;
+    lifePoints /= 2;
+    console.log(lifePoints);
+  }
+  
+  levelUp(): void {
+    this.maxLifePoints += getRandomInt(1, 10);
+    if (this.maxLifePoints >= this.race.maxLifePoints) {
+      this.maxLifePoints = this.race.maxLifePoints;
+    }
+    this._lifePoints = this.maxLifePoints;
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = (attackPoints - this.defense);
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+    if (this.lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this.lifePoints;
+  }
+}
